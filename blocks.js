f00130fea4523e379e526e044723a2311a9e7331
J.P. Ma <J.P. Ma>
2017-02-14 11:51:45 -0500

    Messed up now fixed lul

---
+++ blocks.js
@@ -0,0 +1,9 @@
+function Block() {
+    this.x = random(0, 600);
+    this.y = random(0, 600);
+    this.size = 10;
+    this.show = function() {
+        fill(255);
+        rect(this.x, this.y, this.size, this.size);
+    }
+} 
