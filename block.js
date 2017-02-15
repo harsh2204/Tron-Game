c19a2fd937c06bb732005288d33bcc16fa114ab3
harsh2204 <harshgupta2204@live.com>
2017-02-14 21:43:25 -0500

    Added wall js file and circles around blocks

--- block.js
+++ block.js
@@ -7,7 +7,9 @@ function Block() {
         fill(255);
         rect(this.x, this.y, this.size, this.size);
         noFill();
-        ellipse(this.x,this.y,this.size*2,this.size*2);
+        stroke(255);
+        ellipse(this.x+this.size/2,this.y+this.size/2 ,this.size*2,this.size*2);
+        noStroke();
     }
     this.hits = function(other) {
             var d = dist(this.x, this.y, other.x, other.y);
