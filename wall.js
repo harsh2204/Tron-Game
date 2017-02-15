067e5381227abebeb0601e8ece5c6b86fdf85276
harsh2204 <harshgupta2204@live.com>
2017-02-14 22:44:23 -0500

    Simple optimizations

--- wall.js
+++ wall.js
@@ -1,39 +1,25 @@
 function Wall() {
 
     this.show = function() {
-
-        fill(214,142,255);
+        fill(214, 142, 255);
         noStroke();
         rect(0, 0, 600, 10);
-
-        fill(214,142,255);
-        noStroke();
         rect(0, 0, 10, 600);
-
-        fill(214,142,255);
-        noStroke();
         rect(0, 590, 600, 10);
-
-        fill(214,142,255);
-        noStroke();
         rect(590, 0, 10, 600);
-
-
-
-
     }
-    this.hit = function (other){
-      if(other.x >= 590 ){
-          return true;
-      }
-      if(other.y >= 590){
-          return true;
-      }
-      if(other.y <= 10){
-          return true;
-      }
-      if(other.x <= 10){
-          return true;
-      }
-}
+    this.hit = function(other) {
+        if (other.x >= 590) {
+            return true;
+        }
+        if (other.y >= 590) {
+            return true;
+        }
+        if (other.y <= 10) {
+            return true;
+        }
+        if (other.x <= 10) {
+            return true;
+        }
+    }
 }
